1d065202461bf9e7339856608ed86ce2e4c45b91
goddennis <goddenis@yandex.ru>
2012-04-20 00:22:43 +0700

    app created

--- src/Printer.java
+++ src/Printer.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.applet.Applet;
+import java.awt.*;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
 import java.awt.print.PrinterJob;
 import java.io.IOException;
 
 public class Printer extends Applet {
     public void init() {
-        print("http://google.com");
+        print(getParameter("url"));
     }
 
     public void print(String url) {
@@ -26,6 +29,33 @@ public class Printer extends Applet {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    class HTMLPrinter implements Printable {
+        private final JEditorPane printPane;
+
+        public HTMLPrinter(JEditorPane editorPane) {
+            printPane = editorPane;
+        }
+
+        public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) {
+            if (pageIndex >= 1) return Printable.NO_SUCH_PAGE;
+
+            Graphics2D g2d = (Graphics2D) graphics;
+            g2d.setClip(0, 0, (int) pageFormat.getImageableWidth(), (int) pageFormat.getImageableHeight());
+            g2d.translate((int) pageFormat.getImageableX(), (int) pageFormat.getImageableY());
+
+            RepaintManager rm = RepaintManager.currentManager(printPane);
+            boolean doubleBuffer = rm.isDoubleBufferingEnabled();
+            rm.setDoubleBufferingEnabled(false);
+
+            printPane.setSize((int) pageFormat.getImageableWidth(), 1);
+            printPane.print(g2d);
+
+            rm.setDoubleBufferingEnabled(doubleBuffer);
+
+            return Printable.PAGE_EXISTS;
         }
     }
 }
